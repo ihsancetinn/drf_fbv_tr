f5e951441c8ff49f31b071addbd5ee3b7c112119
aaron <aaron@nioyatech.com>
2021-11-08 21:39:00 +0300

    feat: add todolist endpoint

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, todoList
 
 
 urlpatterns = [
     path('', home),
-    
+    path('todoList/', todoList),  
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -1,7 +1,20 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from rest_framework import serializers
+from .serializers import TodoSerializer
+from .models import Todo
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 def home(request):
     return HttpResponse('<center><h1 style="background-color:powderblue;">Welcome to ApiTodo</h1></center>')
+
+
+@api_view(['GET'])
+def todoList(request):
+    querset = Todo.objects.all()
+    
+    serializer = TodoSerializer(querset, many=True)
+    return Response(serializer.data)
